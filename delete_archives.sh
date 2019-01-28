199a4211d86cba33ecdb83a7996a190ca2f873d0
Tim S <tim.stahlhut@gmail.com>
2019-01-28 09:29:07 -0500

    Add delete_archives.sh

---
+++ delete_archives.sh
@@ -0,0 +1,15 @@
+rm -f autoconf2.59/autoconf-2.59.tar.bz2
+
+rm -f autoconf2.64/autoconf-2.64.tar.gz
+
+rm -f mingw-w64-as6809/av5p10.zip
+
+rm -f mingw-w64-lwtools/lwtools-*.tar.gz
+
+rm -f mingw-w64-gcc6809as/gcc-*.tar.bz2
+
+rm -f mingw-w64-gcc6809lw/gcc-*.tar.bz2
+
+rm -f mingw-w64-newlib6809as/newlib-*.tar.gz
+
+rm -f mingw-w64-newlib6809lw/newlib-*.tar.gz
