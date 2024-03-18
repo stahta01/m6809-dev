456d8b8dbfda07bd234480328ddc707afd6707ff
Tim Stahlhut <stahta01@gmail.com>
2024-03-18 00:35:11 -0400

    script: Update copy-vbcc6809-toolchain-packages.sh

--- copy-vbcc6809-toolchain-packages.sh
+++ copy-vbcc6809-toolchain-packages.sh
@@ -2,8 +2,8 @@ mkdir -p packages/vbcc6809_toolchain_mingw_64_packages
 
 rm -f ./packages/vbcc6809_toolchain_mingw_64_packages/*.zst
 cp -f ./mingw-w64-vlink/mingw-w64-x86_64-vlink-0_17a-1-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-vasm/mingw-w64-x86_64-vasm-1_9d-3-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-vasm/mingw-w64-x86_64-vasm6809-1_9d-3-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-vasm/mingw-w64-x86_64-vasm-1_9f-1-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-vasm/mingw-w64-x86_64-vasm6809-1_9f-1-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-vbcc/mingw-w64-x86_64-vbcc-0_9h-5-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-vbcc/mingw-w64-x86_64-vbcc6809-0_9h-5-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_64_packages/
 
@@ -11,8 +11,8 @@ mkdir -p packages/vbcc6809_toolchain_mingw_32_packages
 
 rm -f ./packages/vbcc6809_toolchain_mingw_32_packages/*.zst
 cp -f ./mingw-w64-vlink/mingw-w64-i686-vlink-0_17a-1-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-vasm/mingw-w64-i686-vasm-1_9d-3-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-vasm/mingw-w64-i686-vasm6809-1_9d-3-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-vasm/mingw-w64-i686-vasm-1_9f-1-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-vasm/mingw-w64-i686-vasm6809-1_9f-1-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-vbcc/mingw-w64-i686-vbcc-0_9h-5-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-vbcc/mingw-w64-i686-vbcc6809-0_9h-5-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_32_packages/
 
@@ -20,7 +20,7 @@ mkdir -p packages/vbcc6809_toolchain_mingw_UCRT64_packages
 
 rm -f ./packages/vbcc6809_toolchain_mingw_UCRT64_packages/*.zst
 cp -f ./mingw-w64-vlink/mingw-w64-ucrt-x86_64-vlink-0_17a-1-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-vasm/mingw-w64-ucrt-x86_64-vasm-1_9d-3-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-vasm/mingw-w64-ucrt-x86_64-vasm6809-1_9d-3-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-vasm/mingw-w64-ucrt-x86_64-vasm-1_9f-1-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-vasm/mingw-w64-ucrt-x86_64-vasm6809-1_9f-1-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-vbcc/mingw-w64-ucrt-x86_64-vbcc-0_9h-5-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-vbcc/mingw-w64-ucrt-x86_64-vbcc6809-0_9h-5-any.pkg.tar.zst ./packages/vbcc6809_toolchain_mingw_UCRT64_packages/
