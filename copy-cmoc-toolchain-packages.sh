14be81a8cac59c5f286a988708f3d6c7fb99633c
Tim Stahlhut <stahta01@gmail.com>
2024-03-19 15:06:51 -0400

    script: Re-write copy-cmoc-toolchain-packages.sh

--- copy-cmoc-toolchain-packages.sh
+++ copy-cmoc-toolchain-packages.sh
@@ -1,20 +1,5 @@
-mkdir -p packages/cmoc_toolchain_mingw_64_packages
+mkdir -p packages/cmoc_toolchain_MSYS_packages
 
-rm -f ./packages/cmoc_toolchain_mingw_64_packages/*.zst
-cp -f ./mingw-w64-lwtools/mingw-w64-x86_64-lwtools-4.22-1-any.pkg.tar.zst ./packages/cmoc_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-cmoc/mingw-w64-x86_64-cmoc-0.1.82-1-any.pkg.tar.zst ./packages/cmoc_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-cmoc-os9/mingw-w64-x86_64-cmoc_os9-git-r85.9f9dfda-4-any.pkg.tar.zst ./packages/cmoc_toolchain_mingw_64_packages/
-
-mkdir -p packages/cmoc_toolchain_mingw_32_packages
-
-rm -f ./packages/cmoc_toolchain_mingw_32_packages/*.zst
-cp -f ./mingw-w64-lwtools/mingw-w64-i686-lwtools-4.22-1-any.pkg.tar.zst ./packages/cmoc_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-cmoc/mingw-w64-i686-cmoc-0.1.82-1-any.pkg.tar.zst ./packages/cmoc_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-cmoc-os9/mingw-w64-i686-cmoc_os9-git-r85.9f9dfda-4-any.pkg.tar.zst ./packages/cmoc_toolchain_mingw_32_packages/
-
-mkdir -p packages/cmoc_toolchain_mingw_UCRT64_packages
-
-rm -f ./packages/cmoc_toolchain_mingw_UCRT64_packages/*.zst
-cp -f ./mingw-w64-lwtools/mingw-w64-ucrt-x86_64-lwtools-4.22-1-any.pkg.tar.zst ./packages/cmoc_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-cmoc/mingw-w64-ucrt-x86_64-cmoc-0.1.82-1-any.pkg.tar.zst ./packages/cmoc_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-cmoc-os9/mingw-w64-ucrt-x86_64-cmoc_os9-git-r85.9f9dfda-4-any.pkg.tar.zst ./packages/cmoc_toolchain_mingw_UCRT64_packages/
+rm -f ./packages/cmoc_toolchain_MSYS_packages/*.zst
+cp -f ./lwtools/lwtools-4.22-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_packages/
+cp -f ./cmoc/cmoc-0.1.86-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_packages/
