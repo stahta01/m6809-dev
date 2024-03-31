9b379c1b2b43e3479f56b5eb1f8fa0b419a0fe38
Tim Stahlhut <stahta01@gmail.com>
2024-03-31 07:42:16 -0400

    script: Update copy-gcc-6.5.0-toolchain-packages.sh

--- copy-gcc-6.5.0-toolchain-packages.sh
+++ copy-gcc-6.5.0-toolchain-packages.sh
@@ -3,21 +3,21 @@ mkdir -p packages/gcc6.5.0_toolchain_mingw_64_packages
 rm -f ./packages/gcc6.5.0_toolchain_mingw_64_packages/*.zst
 cp -f ./mingw-w64-crt-git/mingw-w64-x86_64-headers-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-crt-git/mingw-w64-x86_64-crt-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-gcc6/mingw-w64-x86_64-gcc6-libs-6.5.0-2-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-gcc6/mingw-w64-x86_64-gcc6-6.5.0-2-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc6/mingw-w64-x86_64-gcc6-libs-6.5.0-3-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc6/mingw-w64-x86_64-gcc6-6.5.0-3-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
 
 mkdir -p packages/gcc6.5.0_toolchain_mingw_32_packages
 
 rm -f ./packages/gcc6.5.0_toolchain_mingw_32_packages/*.zst
 cp -f ./mingw-w64-crt-git/mingw-w64-i686-headers-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-crt-git/mingw-w64-i686-crt-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-gcc6/mingw-w64-i686-gcc6-libs-6.5.0-2-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-gcc6/mingw-w64-i686-gcc6-6.5.0-2-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc6/mingw-w64-i686-gcc6-libs-6.5.0-3-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc6/mingw-w64-i686-gcc6-6.5.0-3-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
 
 mkdir -p packages/gcc6.5.0_toolchain_mingw_UCRT64_packages
 
 rm -f ./packages/gcc6.5.0_toolchain_mingw_UCRT64_packages/*.zst
 cp -f ./mingw-w64-crt-git/mingw-w64-ucrt-x86_64-headers-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-crt-git/mingw-w64-ucrt-x86_64-crt-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-gcc6/mingw-w64-ucrt-x86_64-gcc6-libs-6.5.0-2-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-gcc6/mingw-w64-ucrt-x86_64-gcc6-6.5.0-2-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-gcc6/mingw-w64-ucrt-x86_64-gcc6-libs-6.5.0-3-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-gcc6/mingw-w64-ucrt-x86_64-gcc6-6.5.0-3-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_UCRT64_packages/
