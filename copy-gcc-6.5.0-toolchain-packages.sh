fca7a2d817d6ac0e0c8a63b347754ea44b3d7f6e
Tim Stahlhut <stahta01@gmail.com>
2024-04-01 16:16:11 -0400

    script: Update copy-gcc-6.5.0-toolchain-packages.sh

--- copy-gcc-6.5.0-toolchain-packages.sh
+++ copy-gcc-6.5.0-toolchain-packages.sh
@@ -1,6 +1,7 @@
 mkdir -p packages/gcc6.5.0_toolchain_mingw_64_packages
 
 rm -f ./packages/gcc6.5.0_toolchain_mingw_64_packages/*.zst
+cp -f ./mingw-w64-binutils-static-libgcc/mingw-w64-x86_64-*-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-crt-git/mingw-w64-x86_64-headers-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-crt-git/mingw-w64-x86_64-crt-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-gcc6/mingw-w64-x86_64-gcc6-libs-6.5.0-3-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
@@ -9,6 +10,7 @@ cp -f ./mingw-w64-gcc6/mingw-w64-x86_64-gcc6-6.5.0-3-any.pkg.tar.zst ./packages/
 mkdir -p packages/gcc6.5.0_toolchain_mingw_32_packages
 
 rm -f ./packages/gcc6.5.0_toolchain_mingw_32_packages/*.zst
+cp -f ./mingw-w64-binutils-static-libgcc/mingw-w64-i686-*-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-crt-git/mingw-w64-i686-headers-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-crt-git/mingw-w64-i686-crt-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-gcc6/mingw-w64-i686-gcc6-libs-6.5.0-3-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
@@ -17,6 +19,7 @@ cp -f ./mingw-w64-gcc6/mingw-w64-i686-gcc6-6.5.0-3-any.pkg.tar.zst ./packages/gc
 mkdir -p packages/gcc6.5.0_toolchain_mingw_UCRT64_packages
 
 rm -f ./packages/gcc6.5.0_toolchain_mingw_UCRT64_packages/*.zst
+cp -f ./mingw-w64-binutils-static-libgcc/mingw-w64-ucrt-x86_64-*-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-crt-git/mingw-w64-ucrt-x86_64-headers-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-crt-git/mingw-w64-ucrt-x86_64-crt-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-gcc6/mingw-w64-ucrt-x86_64-gcc6-libs-6.5.0-3-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_UCRT64_packages/
