5300d4dac7032324ff2926b7f68e164c4608287f
Tim Stahlhut <stahta01@gmail.com>
2024-08-17 04:46:26 -0400

    script: Update copy-cmoc-toolchain-packages.sh

--- copy-cmoc-toolchain-packages.sh
+++ copy-cmoc-toolchain-packages.sh
@@ -1,6 +1,6 @@
 mkdir -p packages/cmoc_toolchain_MSYS_packages
 
 rm -f ./packages/cmoc_toolchain_MSYS_packages/*.zst
-cp -f ./lwtools/lwtools-4.22-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_packages/
-cp -f ./cmoc/cmoc-0.1.86-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_packages/
+cp -f ./lwtools/lwtools-4.23-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_packages/
+cp -f ./cmoc/cmoc-0.1.88-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_packages/
 cp -f ./cmoc-os9/cmoc_os9-r85.9f9dfda-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_packages/
