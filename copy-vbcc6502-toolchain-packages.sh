d519763e6c10f3a5537b60046fbf06dfc8bc8e0c
Tim Stahlhut <stahta01@gmail.com>
2024-08-17 09:51:48 -0400

    script: Update copy-vbcc6502-toolchain-packages.sh

--- copy-vbcc6502-toolchain-packages.sh
+++ copy-vbcc6502-toolchain-packages.sh
@@ -1,26 +1,8 @@
-mkdir -p packages/vbcc6502_toolchain_mingw_64_packages
+mkdir -p packages/vbcc6502_toolchain_MSYS_packages
 
-rm -f ./packages/vbcc6502_toolchain_mingw_64_packages/*.zst
-cp -f ./mingw-w64-vlink/mingw-w64-x86_64-vlink-0_17a-1-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-vasm/mingw-w64-x86_64-vasm-1_9f-1-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-vasm/mingw-w64-x86_64-vasm6502-1_9f-1-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-vbcc/mingw-w64-x86_64-vbcc-0_9h-6-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-vbcc/mingw-w64-x86_64-vbcc6502-0_9h-6-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_64_packages/
-
-mkdir -p packages/vbcc6502_toolchain_mingw_32_packages
-
-rm -f ./packages/vbcc6502_toolchain_mingw_32_packages/*.zst
-cp -f ./mingw-w64-vlink/mingw-w64-i686-vlink-0_17a-1-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-vasm/mingw-w64-i686-vasm-1_9f-1-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-vasm/mingw-w64-i686-vasm6502-1_9f-1-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-vbcc/mingw-w64-i686-vbcc-0_9h-6-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-vbcc/mingw-w64-i686-vbcc6502-0_9h-6-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_32_packages/
-
-mkdir -p packages/vbcc6502_toolchain_mingw_UCRT64_packages
-
-rm -f ./packages/vbcc6502_toolchain_mingw_UCRT64_packages/*.zst
-cp -f ./mingw-w64-vlink/mingw-w64-ucrt-x86_64-vlink-0_17a-1-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-vasm/mingw-w64-ucrt-x86_64-vasm-1_9f-1-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-vasm/mingw-w64-ucrt-x86_64-vasm6502-1_9f-1-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-vbcc/mingw-w64-ucrt-x86_64-vbcc-0_9h-6-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-vbcc/mingw-w64-ucrt-x86_64-vbcc6502-0_9h-6-any.pkg.tar.zst ./packages/vbcc6502_toolchain_mingw_UCRT64_packages/
+rm -f ./packages/vbcc6502_toolchain_MSYS_packages/*.zst
+cp -f ./vlink/vlink-0_17a-1-any.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_packages/
+cp -f ./vasm/vasm-1_9f-1-any.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_packages/
+cp -f ./vasm/vasm6502-1_9f-1-any.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_packages/
+cp -f ./vbcc/vbcc-0_9h-6-any.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_packages/
+cp -f ./vbcc/vbcc6502-0_9h-6-any.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_packages/
