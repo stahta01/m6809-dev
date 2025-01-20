6d199b69ff3ae34b1c6d813923a8e5194a252807
Tim Stahlhut <stahta01@gmail.com>
2025-01-20 15:26:28 -0500

    script: Edit copy-vbcc6502-toolchain-packages.sh

--- copy-vbcc6502-toolchain-packages.sh
+++ copy-vbcc6502-toolchain-packages.sh
@@ -1,17 +1,17 @@
 mkdir -p packages/vbcc6502_toolchain_MSYS_64bit_packages
 
 rm -f ./packages/vbcc6502_toolchain_MSYS_64bit_packages/*.zst
-cp -f ./vlink/vlink-0_17a-1-x86_64.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_64bit_packages/
-cp -f ./vasm/vasm-2_0a-1-x86_64.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_64bit_packages/
-cp -f ./vasm/vasm6502-2_0a-1-x86_64.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_64bit_packages/
-cp -f ./vbcc/vbcc-0_9h-6-x86_64.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_64bit_packages/
-cp -f ./vbcc/vbcc6502-0_9h-6-x86_64.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./vlink/vlink-0_17a-1-x86_64.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./vasm/vasm-2_0a-1-x86_64.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./vasm/vasm6502-2_0a-1-x86_64.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./vbcc/vbcc-0_9h-6-x86_64.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./vbcc/vbcc6502-0_9h-6-x86_64.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_64bit_packages/
 
 mkdir -p packages/vbcc6502_toolchain_MSYS_32bit_packages
 
 rm -f ./packages/vbcc6502_toolchain_MSYS_32bit_packages/*.zst
-cp -f ./vlink/vlink-0_17a-1-i686.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_32bit_packages/
-cp -f ./vasm/vasm-2_0a-1-i686.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_32bit_packages/
-cp -f ./vasm/vasm6502-2_0a-1-i686.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_32bit_packages/
-cp -f ./vbcc/vbcc-0_9h-6-i686.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_32bit_packages/
-cp -f ./vbcc/vbcc6502-0_9h-6-i686.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./vlink/vlink-0_17a-1-i686.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./vasm/vasm-2_0a-1-i686.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./vasm/vasm6502-2_0a-1-i686.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./vbcc/vbcc-0_9h-6-i686.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./vbcc/vbcc6502-0_9h-6-i686.pkg.tar.zst ./packages/vbcc6502_toolchain_MSYS_32bit_packages/
