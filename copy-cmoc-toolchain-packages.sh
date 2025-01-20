5ece6e43aef3bccf48ba7aa4e939fdc9a4c900f0
Tim Stahlhut <stahta01@gmail.com>
2025-01-20 15:25:42 -0500

    script: Edit copy-cmoc-toolchain-packages.sh

--- copy-cmoc-toolchain-packages.sh
+++ copy-cmoc-toolchain-packages.sh
@@ -1,13 +1,13 @@
 mkdir -p packages/cmoc_toolchain_MSYS_64bit_packages
 
 rm -f ./packages/cmoc_toolchain_MSYS_64bit_packages/*.zst
-cp -f ./lwtools/lwtools-4.23-2-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_64bit_packages/
-cp -f ./cmoc/cmoc-0.1.90-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_64bit_packages/
-cp -f ./cmoc-os9/cmoc_os9-r85.9f9dfda-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./lwtools/lwtools-4.23-2-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./cmoc/cmoc-0.1.90-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./cmoc-os9/cmoc_os9-r85.9f9dfda-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_64bit_packages/
 
 mkdir -p packages/cmoc_toolchain_MSYS_32bit_packages
 
 rm -f ./packages/cmoc_toolchain_MSYS_32bit_packages/*.zst
-cp -f ./lwtools/lwtools-4.23-2-i686.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_32bit_packages/
-cp -f ./cmoc/cmoc-0.1.90-1-i686.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_32bit_packages/
-cp -f ./cmoc-os9/cmoc_os9-r85.9f9dfda-1-i686.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./lwtools/lwtools-4.23-2-i686.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./cmoc/cmoc-0.1.90-1-i686.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./cmoc-os9/cmoc_os9-r85.9f9dfda-1-i686.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_32bit_packages/
