a5c41a1c90cc991fd1bc32e9f100bbb82d65dbca
Tim Stahlhut <stahta01@gmail.com>
2025-08-04 03:21:40 -0400

    script: Update copy-vbcc6809-toolchain-packages.sh

--- copy-vbcc6809-toolchain-packages.sh
+++ copy-vbcc6809-toolchain-packages.sh
@@ -2,8 +2,8 @@ mkdir -p packages/vbcc6809_toolchain_MSYS_64bit_packages
 
 rm -f ./packages/vbcc6809_toolchain_MSYS_64bit_packages/*.zst
 cp --preserve=timestamps -f ./vlink/vlink-0_18-1-x86_64.pkg.tar.zst ./packages/vbcc6809_toolchain_MSYS_64bit_packages/
-cp --preserve=timestamps -f ./vasm/vasm-2_0b-1-x86_64.pkg.tar.zst ./packages/vbcc6809_toolchain_MSYS_64bit_packages/
-cp --preserve=timestamps -f ./vasm/vasm6809-2_0b-1-x86_64.pkg.tar.zst ./packages/vbcc6809_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./vasm/vasm-2_0c-1-x86_64.pkg.tar.zst ./packages/vbcc6809_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./vasm/vasm6809-2_0c-1-x86_64.pkg.tar.zst ./packages/vbcc6809_toolchain_MSYS_64bit_packages/
 cp --preserve=timestamps -f ./vbcc/vbcc-0_9h-6-x86_64.pkg.tar.zst ./packages/vbcc6809_toolchain_MSYS_64bit_packages/
 cp --preserve=timestamps -f ./vbcc/vbcc6809-0_9h-6-x86_64.pkg.tar.zst ./packages/vbcc6809_toolchain_MSYS_64bit_packages/
 
@@ -11,7 +11,7 @@ mkdir -p packages/vbcc6809_toolchain_MSYS_32bit_packages
 
 rm -f ./packages/vbcc6809_toolchain_MSYS_32bit_packages/*.zst
 cp --preserve=timestamps -f ./vlink/vlink-0_18-1-i686.pkg.tar.zst ./packages/vbcc6809_toolchain_MSYS_32bit_packages/
-cp --preserve=timestamps -f ./vasm/vasm-2_0b-1-i686.pkg.tar.zst ./packages/vbcc6809_toolchain_MSYS_32bit_packages/
-cp --preserve=timestamps -f ./vasm/vasm6809-2_0b-1-i686.pkg.tar.zst ./packages/vbcc6809_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./vasm/vasm-2_0c-1-i686.pkg.tar.zst ./packages/vbcc6809_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./vasm/vasm6809-2_0c-1-i686.pkg.tar.zst ./packages/vbcc6809_toolchain_MSYS_32bit_packages/
 cp --preserve=timestamps -f ./vbcc/vbcc-0_9h-6-i686.pkg.tar.zst ./packages/vbcc6809_toolchain_MSYS_32bit_packages/
 cp --preserve=timestamps -f ./vbcc/vbcc6809-0_9h-6-i686.pkg.tar.zst ./packages/vbcc6809_toolchain_MSYS_32bit_packages/
