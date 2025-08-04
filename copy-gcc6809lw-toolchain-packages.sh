83307d65e449b691d75caae3a050a9e4446339ed
Tim Stahlhut <stahta01@gmail.com>
2025-08-04 08:37:21 -0400

    script: Update copy-gcc6809lw-toolchain-packages.sh

--- copy-gcc6809lw-toolchain-packages.sh
+++ copy-gcc6809lw-toolchain-packages.sh
@@ -2,12 +2,12 @@ mkdir -p packages/gcc6809lw_toolchain_MSYS_64bit_packages
 
 rm -f ./packages/gcc6809lw_toolchain_MSYS_64bit_packages/*.zst
 cp --preserve=timestamps -f ./lwtools/lwtools-4.24-1-x86_64.pkg.tar.zst ./packages/gcc6809lw_toolchain_MSYS_64bit_packages/
-cp --preserve=timestamps -f ./gcc6809lw/gcc6809lw-4.3.6-4-x86_64.pkg.tar.zst ./packages/gcc6809lw_toolchain_MSYS_64bit_packages/
-cp --preserve=timestamps -f ./newlib6809lw/m6809-unknown-newlib6809lw-1.16.0-2-x86_64.pkg.tar.zst ./packages/gcc6809lw_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./gcc6809lw/gcc6809lw-4.3.6-5-x86_64.pkg.tar.zst ./packages/gcc6809lw_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./newlib6809lw/m6809-unknown-newlib6809lw-1.16.0-3-x86_64.pkg.tar.zst ./packages/gcc6809lw_toolchain_MSYS_64bit_packages/
 
 mkdir -p packages/gcc6809lw_toolchain_MSYS_32bit_packages
 
 rm -f ./packages/gcc6809lw_toolchain_MSYS_32bit_packages/*.zst
 cp --preserve=timestamps -f ./lwtools/lwtools-4.24-1-i686.pkg.tar.zst ./packages/gcc6809lw_toolchain_MSYS_32bit_packages/
-cp --preserve=timestamps -f ./gcc6809lw/gcc6809lw-4.3.6-4-i686.pkg.tar.zst ./packages/gcc6809lw_toolchain_MSYS_32bit_packages/
-cp --preserve=timestamps -f ./newlib6809lw/m6809-unknown-newlib6809lw-1.16.0-2-i686.pkg.tar.zst ./packages/gcc6809lw_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./gcc6809lw/gcc6809lw-4.3.6-5-i686.pkg.tar.zst ./packages/gcc6809lw_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./newlib6809lw/m6809-unknown-newlib6809lw-1.16.0-3-i686.pkg.tar.zst ./packages/gcc6809lw_toolchain_MSYS_32bit_packages/
