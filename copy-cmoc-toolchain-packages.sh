2abe571523d831ada2dcd95b6c89f4139f9884c0
Tim Stahlhut <stahta01@gmail.com>
2026-08-04 16:31:58 -0400

    script: Update copy-cmoc-toolchain-packages.sh

--- copy-cmoc-toolchain-packages.sh
+++ copy-cmoc-toolchain-packages.sh
@@ -1,13 +1,13 @@
 mkdir -p packages/cmoc_toolchain_MSYS_64bit_packages
 
 rm -f ./packages/cmoc_toolchain_MSYS_64bit_packages/*.zst
-cp --preserve=timestamps -f ./lwtools/lwtools-4.24-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_64bit_packages/
-cp --preserve=timestamps -f ./cmoc/cmoc-0.1.98-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_64bit_packages/
-cp --preserve=timestamps -f ./cmoc-os9/cmoc_os9-r85.9f9dfda-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./lwtools/lwtools-4.25-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./cmoc/cmoc-0.1.99-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./cmoc-os9/cmoc_os9-r257.28e1786-1-x86_64.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_64bit_packages/
 
 mkdir -p packages/cmoc_toolchain_MSYS_32bit_packages
 
 rm -f ./packages/cmoc_toolchain_MSYS_32bit_packages/*.zst
-cp --preserve=timestamps -f ./lwtools/lwtools-4.24-1-i686.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_32bit_packages/
-cp --preserve=timestamps -f ./cmoc/cmoc-0.1.98-1-i686.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_32bit_packages/
-cp --preserve=timestamps -f ./cmoc-os9/cmoc_os9-r85.9f9dfda-1-i686.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./lwtools/lwtools-4.25-1-i686.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./cmoc/cmoc-0.1.99-1-i686.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./cmoc-os9/cmoc_os9-r257.28e1786-1-i686.pkg.tar.zst ./packages/cmoc_toolchain_MSYS_32bit_packages/
